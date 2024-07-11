cc34291369e0c74d248c7a89aa35db94c19b1b49
Vyacheslav-Spilch <spil4evsky@yandex.ru>
2024-07-11 22:16:41 +0300

    rename repository

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio_Stepan_Demin/"
+  base: "portfolio-photographer"
 })
