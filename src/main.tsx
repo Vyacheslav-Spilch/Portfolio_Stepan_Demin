451acd49108d046ea8db9117a667b19a58f790d1
Vyacheslav-Spilch <spil4evsky@yandex.ru>
2024-07-12 11:57:25 +0300

    remove HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 //@ts-ignore
 import App from './App.tsx'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
