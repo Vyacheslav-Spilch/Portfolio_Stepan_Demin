396234d94bf4032101cc60939e3e45eb7639da6c
Vyacheslav-Spilch <spil4evsky@yandex.ru>
2024-07-15 17:25:30 +0300

    refactoring back

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Route } from 'react-router-dom'
 import { lazy, Suspense, useState } from 'react'
 import { Context } from './Context'
 import { getSelectLang } from './utils/GetSelectLang'
+import Layout from './Layout/Layout'
+import GaleryPage from './pages/galery/galeryPage/GaleryPage'
 
 export type SelectLang = 'russian' | 'england'
 
@@ -16,9 +18,6 @@ function App() {
 
   const [ changeLang, setChangeLang ] = useState<string>(getSelectLang('language', 'russian'))
 
-  const Layout = lazy(() => import ('./Layout/Layout'))
-
-  const GaleryPage = lazy(() => import ('./pages/galery/galeryPage/GaleryPage'))
 
   const value: ValueContext = {
     changeLang,
@@ -29,8 +28,8 @@ function App() {
     <div className='main'>
       <Context.Provider value={value}>
         <Routes>
-            <Route path='/portfolio-photographer' element={<Suspense fallback={<h1 color='red'>LOADING...</h1>}><Layout /></Suspense>} />
-            <Route path='/portfolio-photographer/galery/:id' element={<Suspense fallback={<h1 color='red'>LOADING...</h1>}><GaleryPage /></Suspense>}/>
+            <Route path='/portfolio-photographer' element={<Layout />} />
+            <Route path='/galery/:id' element={<GaleryPage />} />
           </Routes> 
       </Context.Provider>
     </div>
